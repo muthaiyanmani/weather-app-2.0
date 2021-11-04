0ea5c3ee016d78cb5749f0f320182f8ffb8eb839
Muthaiyan Mani <muthaiyanmani00@gmail.com>
2021-11-04 10:27:58 +0530

    react tailwind boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div> 
+     <h1 className="text-gray-800 text-center">Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
